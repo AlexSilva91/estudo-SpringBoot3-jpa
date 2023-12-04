51818eaf16e62ccd5d4f5a76e33e1875da591418
Alex da Silva Alves <AlexSilvaAlves17@outlook.com>
2023-12-04 15:48:35 -0300

    User entity and resource

---
+++ src/main/java/com/aulaspringboot/aulaspringboot/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.aulaspringboot.aulaspringboot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aulaspringboot.aulaspringboot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User("Alex", "Alex", "123", "1234");
+		u.setId(1L);
+		return ResponseEntity.ok().body(u);
+	}
+}
